53fd54306ef3983d8dee439d0502254d61422cd6
Bharathhh30 <vnsb30@gmail.com>
2025-02-26 20:43:06 +0530

    Models created

--- config/env.js
+++ config/env.js
@@ -2,6 +2,6 @@ import { config } from "dotenv";
 
 config({path:`.env.${process.env.NODE_ENV || 'development'}.local`});
 // console.log(process.env);
-console.log("Loaded ENV:", process.env.DB_URI);
+// console.log("Loaded ENV:", process.env.DB_URI);
 
 export const {PORT,NODE_ENV , DB_URI} = process.env;
---
+++ models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: [true,'User Name is required'],
+        trim: true,
+        minLength : 2,
+        maxLength : 50,
+    },
+
+    email : {
+        type: String,
+        required: [true,'User Email is required'],
+        trim: true,
+        lowercase:true,
+        unique: true,
+        lowercase: true,
+        match : [/\S+@\S+\.\S+/, 'Please enter a valid email'],
+    },
+
+    password : {
+        type: String,
+        required: [true,'User Password is required'],
+        trim: true,
+        minLength : 6,
+        maxLength : 50,
+    }
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
+// {name : "yashna" email : "by@gmail" password : "password"}
