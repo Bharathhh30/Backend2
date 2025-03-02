7b5786221a343a13ac0661dd832bac48cbbb763c
Bharathhh30 <vnsb30@gmail.com>
2025-03-02 18:09:50 +0530

    added nodemailer

--- config/env.js
+++ config/env.js
@@ -4,4 +4,5 @@ config({path:`.env.${process.env.NODE_ENV || 'development'}.local`});
 // console.log(process.env);
 // console.log("Loaded ENV:", process.env.DB_URI);
 
-export const {PORT,NODE_ENV , DB_URI,JWT_SECRET,JWT_EXPIRE_IN,ARCJET_ENV,ARCJET_KEY} = process.env;
+export const {PORT,NODE_ENV ,
+     DB_URI,JWT_SECRET,JWT_EXPIRE_IN,ARCJET_ENV,ARCJET_KEY,QSTASH_URL,QSTASH_TOKEN,SERVER_URL,EMAIL_PASSWORD} = process.env;
