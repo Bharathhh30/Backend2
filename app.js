a4a64eaeee161fd92fa8f30f50b03b1687bd5b82
Bharathhh30 <vnsb30@gmail.com>
2025-03-02 12:48:17 +0530

    Rate limiting worked

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ import userRouter from "./routes/user.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 
 // means signup route daka vellali ante first api/v1/auth ki piovali
 app.use('/api/v1/auth',authRouter);
